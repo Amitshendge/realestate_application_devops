1d49b4c80a8b1e04229106df75a660ce5d752624
Amitshendge <Amitshendgepro@gmail.com>
2025-03-18 01:51:47 -0700

    commented config

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 
 // Use absolute path for certificates
-export default defineConfig({
-  base: '/auth/',  // Set the base path here
-  plugins: [react()],
-    host: true,
-    port: 5173,
-});
+// export default defineConfig({
+//   base: '/auth/',  // Set the base path here
+//   plugins: [react()],
+//     host: true,
+//     port: 5173,
+// });
