e921536da6d2d96e92d4f3b4e5c02264c685024f
Amitshendge <Amitshendgepro@gmail.com>
2025-03-17 21:45:14 -0700

    replace git_code path

--- devops_scripts/deploy.sh
+++ devops_scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 kill -9 $(lsof -t -i:8500)
 
-nohup streamlit run git_code/app.py --server.port 8500 --server.address 0.0.0.0 > nohup2.out 2>&1 &
+nohup streamlit run git_code_realestate/app.py --server.port 8500 --server.address 0.0.0.0 > nohup2.out 2>&1 &
