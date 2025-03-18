375fdc564dd5bca7ad14fdf56bd9d9d099adabfd
Amitshendge <Amitshendgepro@gmail.com>
2025-03-18 01:57:48 -0700

    as

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 
 // Use absolute path for certificates
-// export default defineConfig({
-//   base: '/',  // Set the base path here
-//   plugins: [react()],
+export default defineConfig({
+  base: '/',  // Set the base path here
+  plugins: [react()],
 //     host: true,
 //     port: 5173,
-// });
+});
