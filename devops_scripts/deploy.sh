658f6a16278005160f906f920280fc8691d427ac
Amitshendge <Amitshendgepro@gmail.com>
2025-03-17 22:02:50 -0700

    update

--- devops_scripts/deploy.sh
+++ devops_scripts/deploy.sh
@@ -1,5 +1,5 @@
 . venv/bin/activate
 
 kill -9 $(lsof -t -i:8000)
-
-nohup uvicorn git_code_realestate/main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
+cd git_code_realestate
+nohup uvicorn main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
