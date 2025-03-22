158224bbdd5c25c120901bd6cf471347957ad1a0
Amitshendge <Amitshendgepro@gmail.com>
2025-03-22 11:22:53 -0700

    transfer env

--- devops_scripts/deploy.sh
+++ devops_scripts/deploy.sh
@@ -1,7 +1,11 @@
 . venv/bin/activate
 
 kill -9 $(lsof -t -i:8500)
+cp .env git_code_realestate/.env
+
 cd git_code_realestate
+
+# Start the FastAPI backend
 nohup uvicorn main:app --host 0.0.0.0 --port 8000 > out.log 2>&1 &
 
 # cd UI
